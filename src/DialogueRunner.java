e8551676d83074fba1d37dcc580fc70a97c00bbb
Bruno Petrus <bruno.petrus@live.com>
2016-12-30 13:00:01 +0100

    Progress on DialogueRunner
    - probably finished method

--- src/DialogueRunner.java
+++ src/DialogueRunner.java
@@ -2,7 +2,10 @@
  * Created by Bruno on 26/12/2016.
  */
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 /**
@@ -21,22 +24,46 @@ public class DialogueRunner {
 
     public void runDialogue() {
         Node currentNode = nodes.get(0);
-        currentNode.display();
-        ArrayList<Link> currentLinks = currentNode.getAvailableLinks();
+        do {
+            currentNode.display();
+            ArrayList<Link> currentLinks = currentNode.getAvailableLinks();
 
-        System.out.println();
-        System.out.println();
+            System.out.println();
+            System.out.println();
 
-        // Print out all the options
-        for(int i = 0; i < currentLinks.size(); i++) {
-            System.out.println(i+1 + ") " + currentLinks.get(i).getDialogueMessage());
-        }
+            // Print out all the options
+            for(int i = 0; i < currentLinks.size(); i++) {
+                System.out.println(i+1 + ") " + currentLinks.get(i).getDialogueMessage());
+            }
 
-        // Asks user to choose a response
+            // Asks user to choose a response
+            int option = getOption(1, currentLinks.size());
 
+            currentLinks.get(option-1).run();
+            currentNode = currentLinks.get(option-1).getNextNode();
+
+            // ENTER LOOP
+        }while (currentNode != null);
     }
 
     private int getOption(int min, int max) {
-        
+        Scanner keyboardInput = new Scanner(System.in);
+
+        System.out.print("Enter a number (" + min + " - " + max + "): ");
+        int option;
+        do {
+            try {
+                option = keyboardInput.nextInt(); // Read the input
+            }catch (InputMismatchException ime) {
+                // ime.printStackTrace(); DEBUG purpose
+                option = -1;
+                System. out.println("You must enter a valid number!");
+            }finally {
+                keyboardInput.nextLine(); // Clean the buffer
+            }
+        }while (option > max || option < min);
+        keyboardInput.close();
+
+        return option;
     }
 }
